e4b08fb97ce7fce4989b8c0b6f55e8df42ea179d
Huma-suhaib <122296566+Huma-suhaib@users.noreply.github.com>
2023-01-13 10:04:12 +0530

    Add files via upload

---
+++ code1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{  
+
+
+int value1, value2 ,value3;
+cin>>value1>>value2>>value3;
+cout<<"Sum is : "<<value1+value2+value3;
+
+    return 0;
+}
